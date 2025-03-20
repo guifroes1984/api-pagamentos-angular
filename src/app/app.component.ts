d7eb1da50e249714a06099f3ca6625c5b1001d9c
guifroes1984 <guilhermefroes26@gmail.com>
2025-03-20 01:50:29 -0300

    Adicionando uma tabela de dados.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'api-pagamentos-front-novo';
+  displayedColumns: string[] = ['pessoa', 'descricao', 'dataVencimento', 'dataPagamento', 'valor'];
+
+  lancamentos = [
+    { descricao: 'Compra de pão', dataVencimento: '30/06/2025', dataPagamento: null, valor: 4.55, pessoa: 'Padaria do José' },
+    { descricao: 'Venda de software', dataVencimento: '10/06/2024', dataPagamento: '09/06/2024', valor: 80000, pessoa: 'Atacado Brasil' },
+    { descricao: 'Impostos', dataVencimento: '20/07/2024', dataPagamento: null, valor: 14312, pessoa: 'Ministério da Fazenda' },
+    { descricao: 'Mensalidade de escola', dataVencimento: '06/05/2024', dataPagamento: '30/05/2024', valor: 800, pessoa: 'Escola Abelha Rainha' },
+    { descricao: 'Venda de carro', dataVencimento: '18/08/2024', dataPagamento: null, valor: 55000, pessoa: 'Sebastião Souza' },
+    { descricao: 'Aluguel', dataVencimento: '18/07/2024', dataPagamento: '09/07/2024', valor: 1750, pessoa: 'Casa Nova Imóveis' },
+    { descricao: 'Mensalidade musculação', dataVencimento: '13/07/2024', dataPagamento: null, valor: 180, pessoa: 'Academia Top' }
+  ];
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { MatButtonModule } from '@angular/material/button';
     MatTabsModule, 
     MatFormFieldModule,
     MatInputModule, 
-    MatButtonModule
+    MatButtonModule, 
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]

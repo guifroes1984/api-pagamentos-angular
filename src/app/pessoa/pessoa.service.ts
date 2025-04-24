eeff1ea2660d3a1c6d0a82055ea905e9c3d8cabb
guifroes1984 <guilhermefroes26@gmail.com>
2025-04-24 01:06:25 -0300

    Configurando a paginação no serviço de pessoas.

--- src/app/pessoa/pessoa.service.ts
+++ src/app/pessoa/pessoa.service.ts
@@ -1,9 +1,13 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
 import { firstValueFrom } from 'rxjs';
+import * as moment from 'moment';
 
 export class NomeFiltro {
   nome?: string;
+  pagina: number = 0;
+  itensPorPagina: number = 5;
 }
 
 @Injectable({
@@ -16,10 +20,13 @@ export class PessoaService {
   constructor(private http: HttpClient) { }
 
   pesquisar(filtro: NomeFiltro): Promise<any> {
+    let params = new HttpParams();
     const headers = new HttpHeaders()
       .set('Authorization', 'Basic YWRtaW5AYWRtaW4uY29tOmFkbWlu');
-  
-    let params = new HttpParams();
+
+    params = params.set('page', filtro.pagina.toString());
+    params = params.set('size', filtro.itensPorPagina.toString());
+
     if (filtro.nome) {
       params = params.set('nome', filtro.nome);
     }
--- src/app/pessoa/pessoas-pesquisa/pessoas-pesquisa.component.ts
+++ src/app/pessoa/pessoas-pesquisa/pessoas-pesquisa.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatPaginator } from '@angular/material/paginator';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
-import { PessoaService } from '../pessoa.service';
+import { NomeFiltro, PessoaService } from '../pessoa.service';
 
 @Component({
   selector: 'app-pessoas-pesquisa',
@@ -10,39 +10,53 @@ import { PessoaService } from '../pessoa.service';
 })
 export class PessoasPesquisaComponent implements OnInit {
 
-  constructor(private pessoaService: PessoaService) { }
-  
+  fonteDados = new MatTableDataSource<any>();
   colunasExibidas: string[] = ['pessoa', 'cidade', 'estado', 'status', 'acoes'];
-  
-  pessoas = [];
 
-  filtro: any = {};
-  
-  fonteDados = new MatTableDataSource(this.pessoas);
-  
+  totalRegistros = 0;
+  itensPorPagina = 5;
+  paginaAtual = 0;
+
+  filtro = new NomeFiltro();
+
   @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  constructor(private pessoaService: PessoaService) {}
 
   ngOnInit(): void {
     this.pesquisar();
   }
-  
-  ngAfterViewInit() {
-    this.fonteDados.paginator = this.paginator;
+
+  pesquisar(pagina: number = 0): void {
+    this.filtro.pagina = pagina;
+    this.filtro.itensPorPagina = this.itensPorPagina;
+
+    this.pessoaService.pesquisar(this.filtro).then(resultado => {
+      this.fonteDados.data = resultado.content;
+      this.totalRegistros = resultado.totalElements;
+      this.paginaAtual = resultado.number;
+      if (this.paginator) {
+        this.paginator.pageIndex = this.paginaAtual;
+      }
+    });
   }
 
-  pesquisar() {
-    this.pessoaService.pesquisar(this.filtro)
-      .then(response => {
-        this.pessoas = response.content;
-        this.fonteDados.data = response.content;
-      });
+  habilitarPaginacao(): boolean {
+    return this.totalRegistros > 5;
   }
 
-  limpar() {
-    this.filtro.nome = '';
-    this.paginator.firstPage();
+  onPaginar(event: PageEvent): void {
+    this.itensPorPagina = event.pageSize;
+    this.paginaAtual = event.pageIndex;
+    this.pesquisar(this.paginaAtual);
+  }
+
+  limpar(): void {
+    this.filtro = new NomeFiltro();
+    this.itensPorPagina = 5;
+    this.paginaAtual = 0;
+    this.fonteDados.data = [];
     this.pesquisar();
   }
-  
 
 }

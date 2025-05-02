b3bb1ca6faea3d5282ce31b4a3599849aaa588c6
guifroes1984 <guilhermefroes26@gmail.com>
2025-05-01 23:38:52 -0300

    Criando classe de modelo e usando no cadastro de pessoas.

--- src/app/pessoa/pessoas-cadastro/pessoas-cadastro.component.ts
+++ src/app/pessoa/pessoas-cadastro/pessoas-cadastro.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Pessoa } from 'src/app/core/model/pessoa';
 
 @Component({
   selector: 'app-pessoas-cadastro',
@@ -7,17 +8,19 @@ import { Component } from '@angular/core';
 })
 export class PessoasCadastroComponent {
 
-  nomeModel: string = '';
-  logradouroModel: string = '';
-  numeroModel: string = '';
-  bairroModel: string = '';
-  cepModel: string = '';
-  cidadeModel: string = '';
-  estadoModel: string = '';
-  complementoModel: string = '';
+  // nomeModel: string = '';
+  // logradouroModel: string = '';
+  // numeroModel: string = '';
+  // bairroModel: string = '';
+  // cepModel: string = '';
+  // cidadeModel: string = '';
+  // estadoModel: string = '';
+  // complementoModel: string = '';
+
+  public pessoa = new Pessoa();
 
   salvar() {
-    
+    console.log(this.pessoa);
   }
 
 }

fce7566f33e95082efba5cc61b129b6ac47158b2
guifroes1984 <guilhermefroes26@gmail.com>
2025-05-07 23:14:34 -0300

    Ajuste para não aparecer na tela de login o Navbar.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,14 @@ import { MatTableDataSource } from '@angular/material/table';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  constructor(
+    private router: Router
+  ) { }
+
+  public exibindoNavbar() {
+    return this.router.url !== '/login';
+  }
 
 }
 

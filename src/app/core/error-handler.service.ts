0169e43af0c80fa61f2630e2c898b0cea3397a23
guifroes1984 <guilhermefroes26@gmail.com>
2025-05-17 00:31:51 -0300

    Tratando se o Refresh Token expirar.

--- src/app/core/error-handler.service.ts
+++ src/app/core/error-handler.service.ts
@@ -1,25 +1,33 @@
 import { Injectable } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { HttpErrorResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ErrorHandlerService {
 
-  constructor(private toastr: ToastrService) { }
+  constructor(
+    private toastr: ToastrService, 
+    private router: Router
+  ) { }
 
   handle(errorResponse: any) {
-    let msg = '';
+  let msg = '';
 
-    if (typeof errorResponse === 'string') {
-      msg = errorResponse;
-    } else if (errorResponse instanceof HttpErrorResponse && errorResponse.status >= 400 && errorResponse.status <= 499) {
+  if (typeof errorResponse === 'string') {
+    msg = errorResponse;
+
+  } else if (errorResponse instanceof HttpErrorResponse) {
+    if (errorResponse.status === 401) {
+      msg = 'Sua sessão expirou!';
+      this.router.navigate(['/login']);
+
+    } else if (errorResponse.status === 403) {
+      msg = 'Você não tem permissão para executar a sua solicitação.';
+    } else if (errorResponse.status >= 400 && errorResponse.status <= 499) {
       msg = 'Ocorreu um erro ao processar a sua solicitação.';
-
-      if (errorResponse.status === 403) {
-        msg = 'Você não tem permissão para executar a sua solicitação.';
-      }
 
       try {
         const errors = errorResponse.error;
@@ -29,13 +37,18 @@ export class ErrorHandlerService {
         }
       } catch (e) { }
 
-      console.error('Erro:', errorResponse);
     } else {
       msg = 'Erro ao processar serviço remoto. Tente novamente.';
-      console.error('Erro:', errorResponse);
     }
 
-    this.toastr.error(msg);
+    console.error('Erro:', errorResponse);
+
+  } else {
+    msg = 'Erro ao processar serviço remoto. Tente novamente.';
+    console.error('Erro:', errorResponse);
+  }
+
+  this.toastr.error(msg);
   }
 
 }

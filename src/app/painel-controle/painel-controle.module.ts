ad13ce8f9ee15badb70f6b7df8ed37ff23eb4eb6
guifroes1984 <guilhermefroes26@gmail.com>
2025-06-09 02:08:02 -0300

    Criando gráfico com dados estáticos.

--- src/app/painel-controle/painel-controle.module.ts
+++ src/app/painel-controle/painel-controle.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { NgChartsModule } from 'ng2-charts';
+import { MatCardModule } from '@angular/material/card';
+
+
 import { SharedModule } from '../shared/shared.module';
 import { PainelControleRoutingModule } from './painel-controle-routing.module';
 import { PainelControleComponent } from './painel-controle/painel-controle.component';
@@ -12,6 +16,9 @@ import { PainelControleComponent } from './painel-controle/painel-controle.compo
   ],
   imports: [
     CommonModule, 
+
+    NgChartsModule, 
+    MatCardModule, 
     
     SharedModule, 
     PainelControleRoutingModule
--- src/app/painel-controle/painel-controle/painel-controle.component.ts
+++ src/app/painel-controle/painel-controle/painel-controle.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-painel-controle',
   templateUrl: './painel-controle.component.html',
   styleUrls: ['./painel-controle.component.css']
 })
-export class PainelControleComponent {
+export class PainelControleComponent implements OnInit {
+
+  dadosGraficoPizza = {
+    labels: ['Mensal', 'Educação', 'Lazer', 'Imprevistos'],
+    datasets: [
+      {
+        data: [2500, 2700, 550, 235],
+        backgroundColor: ['#FF9900', '#109618', '#990099', '#3B3EAC']
+      }
+    ]
+  };
+
+  dadosGraficoLinha = {
+    labels: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
+    datasets: [
+      {
+        label: 'Receitas',
+        data: [4, 10, 18, 5, 1, 20, 3],
+        borderColor: '#3366CC',
+        fill: false
+      },
+      {
+        label: 'Despesas',
+        data: [10, 15, 8, 5, 1, 7, 9],
+        borderColor: '#D62B00',
+        fill: false
+      }
+    ]
+  };
+
+  constructor() {}
+
+  ngOnInit(): void {}
 
 }

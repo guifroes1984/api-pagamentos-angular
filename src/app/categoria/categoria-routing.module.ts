4e86065e76b5e4007324ead539fc97ff12e40d55
guifroes1984 <guilhermefroes26@gmail.com>
2025-12-03 00:58:18 -0300

    Fazendo o lazy module routes.

--- src/app/categoria/categoria-routing.module.ts
+++ src/app/categoria/categoria-routing.module.ts
@@ -1,7 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoriaListagemComponent } from './categoria-listagem/categoria-listagem.component';
+import { AuthGuard } from '../seguranca/auth.guard';
+import { CategoriaCadastroComponent } from './categoria-cadastro/categoria-cadastro.component';
+import { PendenciasGuard } from '../core/guards/FormComPendencias';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: CategoriaListagemComponent,
+    canActivate: [AuthGuard], 
+    canDeactivate: [PendenciasGuard],
+    data: { roles: ['ROLE_PESQUISAR_CATEGORIA'], 
+    title: 'Categorias' }  
+  }, 
+  {
+    path: 'nova',
+    component: CategoriaCadastroComponent,
+    canActivate: [AuthGuard],
+    canDeactivate: [PendenciasGuard],
+    data: { roles: ['ROLE_CADASTRAR_CATEGORIA'],
+    title: 'Nova Categoria' }
+  }, 
+  {
+    path: ':codigo',
+    component: CategoriaCadastroComponent,
+    canActivate: [AuthGuard], 
+    canDeactivate: [PendenciasGuard],
+    data: { roles: ['ROLE_CADASTRAR_CATEGORIA'],
+    title: 'Editar Categoria' }
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
